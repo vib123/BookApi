778c6b65c77949b17cb429f2b9dd6cafaf2275ae
vibhor-barnwal <vbhr.kmar@gmail.com>
2025-03-30 15:51:11 +0530

    api configuration updated

--- src/main/java/book/example/BookApi/controller/BookController.java
+++ src/main/java/book/example/BookApi/controller/BookController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/v1")
 public class BookController {
 
     private final BookService bookService;
